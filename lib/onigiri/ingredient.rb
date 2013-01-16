c0efe75460a0c6b3c2e990c132766889ab5e09fd
robodisco <adamsubscribe@googlemail.com>
2013-01-16 19:34:25 +0900

    ingredient tags tokens

--- lib/onigiri/ingredient.rb
+++ lib/onigiri/ingredient.rb
@@ -3,9 +3,27 @@ module Onigiri
     class << self
       attr_reader :ingredients
 
+      def scan(tokens)
+        tokens.each do |token|
+          scan_for_ingredient token
+        end
+      end
+
+      def scan_for_ingredient(token)
+        ingredients.each do |correct_form, variations|
+          variations.each do |var|
+            if token.name == var
+              token.add_tag :ingredient
+              return
+            end
+          end
+        end
+      end
+
       def set_ingredient(correct_form, *variations)
         @ingredients ||={}
         @ingredients[correct_form] = variations
+        @ingredients[correct_form].push correct_form #add the correct_form to variations to ease token matching
       end
     end
   end
--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -7,4 +7,11 @@ class TestIngredient < MiniTest::Unit::TestCase
     assert Onigiri::Ingredient.ingredients.has_key? 'banana'
     assert Onigiri::Ingredient.ingredients['banana'].include? 'bananaz'
   end
+
+  def test_tags_tokens
+    Onigiri::Ingredient.set_ingredient 'banana'
+    tok_a = Onigiri::Token.new("banana")
+    Onigiri::Ingredient.scan([tok_a])
+    assert_equal true, tok_a.tags.include?(:ingredient)
+  end
 end
