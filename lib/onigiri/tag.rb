bedc19f8624b2310d3193bb93354da26b5e10798
robodisco <adamsubscribe@googlemail.com>
2013-01-17 17:47:33 +0900

    fixed - make type an instance variable and not class for Tag class.

--- lib/onigiri/tag.rb
+++ lib/onigiri/tag.rb
@@ -1,11 +1,8 @@
 module Onigiri
   class Tag
-    class << self
-      attr_accessor :type
-
-      def initialize(type)
-        @type = type
-      end
+    attr_accessor :type
+    def initialize(type)
+      @type = type
     end
   end
 end
