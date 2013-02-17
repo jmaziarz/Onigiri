34caeeed17a5f4b239bea9321cb6201330dcf131
robodisco <adamsubscribe@googlemail.com>
2013-02-17 11:23:17 +0900

    reedit ing tests to reflect new changes

--- test/ingredient_test.rb
+++ test/ingredient_test.rb
@@ -4,13 +4,13 @@ require 'helper'
 class TestIngredient < MiniTest::Unit::TestCase
   def test_sets_ingredient
     Onigiri::Ingredient.set_ingredient 'banana', 'bananaz'
-    assert Onigiri::Ingredient.ingredients.has_key? 'banana'
-    assert Onigiri::Ingredient.ingredients['banana'].include? 'bananaz'
+    assert Onigiri::Ingredient.ingredients[1].has_key? 'bananaz'
+    assert_equal 'banana', Onigiri::Ingredient.ingredients[1]['bananaz']
   end
 
-  def test_sets_ingredients_of_multiple_words
+  def test_sets_ingredients_by_their_word_count
     Onigiri::Ingredient.set_ingredient 'cherry tomato'
-    assert Onigiri::Ingredient.ingredients.has_key? 'cherry tomato'
+    assert Onigiri::Ingredient.ingredients[2].has_key? 'cherry tomato'
   end
 
   def test_underscores_multi_word_ingredients_to_normalize
@@ -19,18 +19,19 @@ class TestIngredient < MiniTest::Unit::TestCase
     assert_equal "10 lbs of cherry_tomato", Onigiri::Ingredient.normalize(text)
   end
 
-  def test_tags_tokens
-    Onigiri::Ingredient.set_ingredient 'banana', 'bazza'
-    tok_a = Onigiri::Token.new("bazza")
+  def test_normalizes_ingredients_with_regexps
+    string = 'testABCDtest'
+    Onigiri::Ingredient.set_ingredient 'success', 'test\w\w\w\wtest'
+    result = Onigiri::Ingredient.normalize(string)
+    assert_equal 'success', result
+  end
+
+  def test_tags_tokens_with_correct_form_name
+    Onigiri::Ingredient.set_ingredient 'banana'
+    tok_a = Onigiri::Token.new("banana")
     Onigiri::Ingredient.scan_for_ingredient(tok_a)
     assert_equal true, tok_a.tags.first.is_a?(Onigiri::Ingredient)
     assert_equal "banana", tok_a.tags.first.type
   end
 
-  def test_can_use_regexs_defined_in_set_ingredients_when_scanning
-    Onigiri::Ingredient.set_ingredient 'success', 'test\w\w\w\wtest'
-    tok_a = Onigiri::Token.new("testABCDtest")
-    Onigiri::Ingredient.scan_for_ingredient(tok_a)
-    assert "success", tok_a.get_tag(Onigiri::Ingredient).type 
-  end
 end
